c6feab2b6c23887cbd8194558db8880e81903715
Andrew Yang <evil_eve_live@outlook.com>
2019-05-07 11:38:17 +0800

    update filter for rainbows

--- app/admin/rainbows.rb
+++ app/admin/rainbows.rb
@@ -19,6 +19,24 @@ permit_params do
   permitted
 end
 
+
+  filter :sentence
+  filter :explain
+  filter :tagged_with_type,
+         as: :select,
+         collection: -> {Rainbow.type_counts.map(&:name) },
+         label: 'Types'
+
+  filter :tagged_with_feature,
+         as: :select,
+         collection: -> {Rainbow.feature_counts.map(&:name)},
+         label: 'Features'
+
+  filter :tagged_with_keyword,
+         as: :select,
+         collection: -> { Rainbow.keyword_counts.map(&:name)},
+         label: 'Keywords'
+
   index do |list|
     selectable_column
     id_column
@@ -26,6 +44,7 @@ end
     column :explain
     column :feature_list
     column :type_list
+    column :collection_list
     actions
 
   end
@@ -38,6 +57,7 @@ end
       f.input :gender_list
       f.input :feature_list
       f.input :type_list
+      f.input :collection_list
     end
 
     f.actions
--- app/models/rainbow.rb
+++ app/models/rainbow.rb
@@ -1,8 +1,18 @@
 class Rainbow < ApplicationRecord
-acts_as_taggable_on :genders,:keywords, :features, :types
+  acts_as_taggable_on :genders,:keywords, :features, :types, :collections # collection用来给句子归类，写一个标题, 比如"祝寿彩虹屁大集合"
 
-  scope :poisonous_soup,  ->{ Rainbow.tagged_with("毒鸡汤", on: :types)}
-  scope :rainbowfart,     ->{Rainbow.tagged_with("彩虹屁", on: :types)}
-  scope :golden_sentence, ->{Rainbow.tagged_with("金句", on: :types)}
-  scope :famous_quotes,   ->{Rainbow.tagged_with("名人名言", on: :types)}
+  scope :poisonous_soup,  ->{ tagged_with("毒鸡汤", on: :types)}
+  scope :rainbowfart,     ->{ tagged_with("彩虹屁", on: :types)}
+  scope :golden_sentence, ->{ tagged_with("金句", on: :types)}
+  scope :famous_quotes,   ->{ tagged_with("名人名言", on: :types)}
+
+  scope :tagged_with_type, ->(*tag) { tagged_with(tag, on: :types)}
+  scope :tagged_with_feature, ->(*feature) { tagged_with(feature, on: :features)}
+  scope :tagged_with_keyword, ->(*keyword) {tagged_with(keyword, on: :keyword)}
+
+  # search_methods :tagged_with_type
+
+  def self.ransackable_scopes(auth=nil)
+    %i(tagged_with_type tagged_with_feature tagged_with_keyword)
+  end
 end
