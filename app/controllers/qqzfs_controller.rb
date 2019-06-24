e7266797601e173a159276c8a09c84fd83023e1c
Andrew Yang <evil_eve_live@outlook.com>
2019-06-24 22:22:18 +0800

    remove ransack out of front end page to speed up search.

--- app/controllers/qqzfs_controller.rb
+++ app/controllers/qqzfs_controller.rb
@@ -2,9 +2,9 @@ class QqzfsController < InheritedResources::Base
 
   def index
 
-    @q = Qqzf.ransack(params[:q])
-    @qqzfs = @q.result()
+    # @q = Qqzf.ransack(params[:q])
+    # @qqzfs = @q.result()
 
-    @qqzfs = @qqzfs.page(params[:page]).per(25)
+    @qqzfs = Qqzf.all.page(params[:page]).per(25)
   end
 end
