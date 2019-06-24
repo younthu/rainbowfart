172913b9cf91b1b9e87e1ee35663866a248e4589
Andrew Yang <evil_eve_live@outlook.com>
2019-06-24 23:25:23 +0800

    update sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "http://rainbow.ilibrary.me"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
