fc66a14dfe56bd2635362b1c6fd4823fe4dddbfe
Zhiyong Yang <evil_eve_live@outlook.com>
2020-08-08 18:54:23 +0800

    fix number format.

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,5 +1,5 @@
 Kaminari.configure do |config|
   config.default_per_page = 25
   config.max_per_page = 100
-  config.max_pages = 10,000,000 # active admin后台也用了kaminari
+  config.max_pages = 10_000_000 # active admin后台也用了kaminari
 end
